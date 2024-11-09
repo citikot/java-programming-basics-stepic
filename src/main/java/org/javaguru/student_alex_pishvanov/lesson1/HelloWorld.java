27beaf39d33226102aa1c05c5ebc7874b6a927f2
Alex Pishvanov <citikot@dr.com>
2024-11-09 23:01:02 +0200

    added lesson1 homework

---
+++ src/main/java/org/javaguru/student_alex_pishvanov/lesson1/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_alex_pishvanov.lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
